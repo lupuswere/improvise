008229ee95b6955dbea20ede9a47592caebbb96e
Lifei Li <lupuswere@gmail.com>
2015-04-20 16:32:39 -0700

    first POST

---
+++ Improvise-Prototype-iOS/Improvise-Prototype-iOS/Models/AcceptedInvitations.h
@@ -0,0 +1,13 @@
+//
+//  AcceptedInvitations.h
+//  Improvise-Prototype-iOS
+//
+//  Created by Lifei Li on 15/4/20.
+//  Copyright (c) 2015年 Lifei Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AcceptedInvitations : NSObject
+
+@end
---
+++ Improvise-Prototype-iOS/Improvise-Prototype-iOS/Models/AcceptedInvitations.m
@@ -0,0 +1,13 @@
+//
+//  AcceptedInvitations.m
+//  Improvise-Prototype-iOS
+//
+//  Created by Lifei Li on 15/4/20.
+//  Copyright (c) 2015年 Lifei Li. All rights reserved.
+//
+
+#import "AcceptedInvitations.h"
+
+@implementation AcceptedInvitations
+
+@end
