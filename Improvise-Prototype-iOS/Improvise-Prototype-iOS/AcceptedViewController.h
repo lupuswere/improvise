809483e4d16068fab82e6640a82e54b6e89e27a7
Lifei Li <lupuswere@gmail.com>
2015-04-19 17:18:32 -0700

    four controllers added

---
+++ Improvise-Prototype-iOS/Improvise-Prototype-iOS/AcceptedViewController.h
@@ -0,0 +1,13 @@
+//
+//  AcceptedViewController.h
+//  Improvise-Prototype-iOS
+//
+//  Created by Lifei Li on 15/4/19.
+//  Copyright (c) 2015年 Lifei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AcceptedViewController : UIViewController
+
+@end
---
+++ Improvise-Prototype-iOS/Improvise-Prototype-iOS/AcceptedViewController.m
@@ -0,0 +1,37 @@
+//
+//  AcceptedViewController.m
+//  Improvise-Prototype-iOS
+//
+//  Created by Lifei Li on 15/4/19.
+//  Copyright (c) 2015年 Lifei Li. All rights reserved.
+//
+
+#import "AcceptedViewController.h"
+
+@interface AcceptedViewController ()
+
+@end
+
+@implementation AcceptedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
