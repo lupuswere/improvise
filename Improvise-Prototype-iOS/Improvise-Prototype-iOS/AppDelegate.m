b95f6b2e4149529a680e416678b45d4d51488b03
Lifei Li <lupuswere@gmail.com>
2015-05-04 22:37:07 -0700

    push notifications

--- Improvise-Prototype-iOS/Improvise-Prototype-iOS/AppDelegate.m
+++ Improvise-Prototype-iOS/Improvise-Prototype-iOS/AppDelegate.m
@@ -34,13 +34,6 @@
     return YES;
 }
 
-- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-    NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
-    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
-    NSLog(@"content---%@", token);
-}
-
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
